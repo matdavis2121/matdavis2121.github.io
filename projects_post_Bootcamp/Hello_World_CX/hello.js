02ed73cbe54fdd4acf163b8d36873c59550bf9ee
Matthew Davis <matdavis2121@gmail.com>
2017-08-29 06:43:12 +0000

    Improved CSS and animation features using iteration event and animation end event.

--- projects_post_Bootcamp/Hello_World_CX/hello.js
+++ projects_post_Bootcamp/Hello_World_CX/hello.js
@@ -7,16 +7,22 @@ MULTIPLE MISTAKES1
 3. db inside outside of success??? prolly not the real issue
 4. 
 */
-
+console.groupCollapsed("Hello_World_CX")
 var versNum = 5
 
-if(window.indexedDB.open("Hello_World_CX") !== undefined){createNewDB()}
+createNewDB()
+
 
 function lo(s1,s2,s3){
     if(s2 === undefined){s2 = ""}
     if(s3 === undefined){s3 = ""}
-    console.trace(s1,s2,s3)
+    
+    
+    console.groupCollapsed()
+    console.trace(s1,s2 || "",s3 || "")
+    console.groupEnd()
 }
+
 
 //1. LOAD EventListener
 window.addEventListener("load", function(){    
@@ -24,26 +30,44 @@ window.addEventListener("load", function(){
     
     //animation iteration setter
     dbCount()
-    var entryCounter = 0
+    var entryCounter = 1
     var entries = getAllEntries()
+
+    //set first entry as the first INNERTEXT
+    if(localStorage.firstEntry){world.innerText = localStorage.firstEntry;world.style.color = "green"}
+    else{world.innerText = "______";world.style.color = "green"}
     
-    world.style.WebkitAnimation = "allWorlds 3s " + Number(localStorage.entryCount)
+    //set webkitanimation style values
+    world.style.WebkitAnimation = "allWorlds 2s " + (Number(localStorage.entryCount)+1)
+    world.style.animationFillMode = "forwards"
+    
+    //animation iteration event listener
     world.addEventListener("webkitAnimationIteration", function(e){
-        //lo("animation repeated");
         
+        if(entryCounter === Number(localStorage.entryCount)) {
+            world.style.animationName = "allWorldsReset"
+            world.style.animationIterationCount = "1"
+        }
         this.innerText = entries[entryCounter]
         entryCounter += 1
     });
     
+    //animation END event listener
+    world.addEventListener("webkitAnimationEnd", function(e){
+        e.preventDefault = true
+        this.style.opacity = "1"
+        //this.innerText = "complete"//localStorage.worldName
+        world.style.animationFillMode = "none"
+        //world.style.animationName= "allWorldsReset"
+        
+    });
     
-    if(localStorage.worldName){world.innerText = localStorage.worldName;world.style.color = "green"}
-    else{world.innerText = "______"}
-    
+    //Input Element INPUT Event Listener
     var worldName = document.querySelector("#world-input")
     worldName.addEventListener("input", function(){
         if(this.value === ""){
             world.innerText = "________"
-            world.style.color = "black"
+            world.style.color = "green"
         }
         else if(this.value){
             world.innerText = this.value
@@ -52,9 +76,23 @@ window.addEventListener("load", function(){
             else{console.log("Storage support not available")}
         }
     })
+    
+    //Input CLICK Event to stop the animation when the INPUT ELEMENT is clicked on
+    localStorage.clickOnce = false
+    worldName.addEventListener("click", function(e){
+        var clickOnce = JSON.parse(localStorage.clickOnce)
+        if(!clickOnce){
+            if(localStorage.worldName){world.innerText = localStorage.worldName;world.style.color = "green"}
+            else{world.innerText = "______"}
+            
+            world.style.WebkitAnimation = "allWorlds 3s " + 0
+            
+            localStorage.clickOnce = true
+        }
+    })
 
 
-    //2. Input Keydown EventListener
+    //Input Element KEYDOWN EventListener - adds the Inputs innertext to INDEXEDDB when user presses enter
     document.querySelector("#world-input").addEventListener("keydown", function(e){
         if(e.key === "Enter"){
             addEntryIDB(this.value)
@@ -72,7 +110,7 @@ function createNewDB(){
     var s1 = "success", e1 = "error", u1 = "upgraded"
     var DB
     
-    openDB.onupgradeneeded = function(e){lo(u1); DB = e.target.result; DB.createObjectStore("KeydownEntries"); DB.close()}
+    openDB.onupgradeneeded = function(e){lo(u1); DB = e.target.result; DB.createObjectStore("KeydownEntries"); DB.close() }
     openDB.onsuccess =       function(e){lo(s1); DB = e.target.result}
     openDB.onerror =         function(e){lo(e1)}
         
@@ -158,4 +196,31 @@ function getAllEntries(){
 return entries
 }
 
+//3Fe. Get First of Entries -- **CURRENTLY UNUSED** Issue with returning undefined
+function getFirstEntry(){
+    var openDB = window.indexedDB.open("Hello_World_CX")
+    var res
+    var entryCount = null
+	var entry
+    
+    openDB.onsuccess = function(e){res = e.target.result;
+    
+    var trans = res.transaction(["KeydownEntries"], "readonly").objectStore("KeydownEntries")
+    var count = trans.get(1)
+    	count.onsuccess =  function(e){ 
+			var cursor = e.target.result;  
+			if(cursor){localStorage.firstEntry = cursor; }
+			else{console.log("%cnoFirst Entry","color:red;font-size:20px")}
+			
+        }
+	res.close()     
+    }
+//delete entries[entries.length - 1]    
+return entry
+}
 
+
+
+
+//turn into a button?
+//console.groupEnd()
